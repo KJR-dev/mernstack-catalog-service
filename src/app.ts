532a84898720fc829de46bdfd94ee9156372103c
Jitendra Sahoo <sahooj168@gmail.com>
2025-11-13 16:26:24 +0530

    setup file base configuration

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response } from "express";
+import config from 'config';
 import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World!");
+    res.status(200).json({ message: "Hello from catalog service!" });
 });
 
 app.use(globalErrorHandler);
