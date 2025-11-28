81f3252760ee80ad9e573eeb9e7ebd8bfe8a1052
Jitendra Sahoo <sahooj168@gmail.com>
2025-11-28 10:38:02 +0530

    added wrapper

--- src/category/catgory-router.ts
+++ src/category/catgory-router.ts
@@ -3,11 +3,16 @@ import { CategoryController } from "./category-controller";
 import categoryValidator from "./category-validator";
 import { CategoryService } from "./category-service";
 import logger from "../config/logger";
+import { asyncWrapper } from "../common/utils/wrapper";
 
 const categoryRouter = express.Router();
 const categoryService = new CategoryService();
 const categoryController = new CategoryController(categoryService, logger);
 
-categoryRouter.post("/", categoryValidator, categoryController.create);
+categoryRouter.post(
+    "/",
+    categoryValidator,
+    asyncWrapper(categoryController.create),
+);
 
 export default categoryRouter;
